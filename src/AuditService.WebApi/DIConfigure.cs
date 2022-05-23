349acebbf52fee925a4fdbd769385185e787fc5f
afinderfall <afinder@bk.ru>
2022-05-23 18:15:20 +0600

    Задачи LOG-3 и LOG-4

--- src/AuditService.WebApi/DIConfigure.cs
+++ src/AuditService.WebApi/DIConfigure.cs
@@ -1,9 +1,32 @@
+using Elasticsearch.Net;
+using Nest;
+
 namespace AuditService.WebApi;
 
-public static class DIConfigure
+public static class DiConfigure
 {
-    public static IServiceCollection Configure(IServiceCollection services)
+    public static IServiceCollection Configure(IServiceCollection services, IConfiguration ñonfiguration)
     {
+        RegisterElasticSearch(services, ñonfiguration);
+
         return services;
+    }
+
+    /// <summary>
+    ///     Çàðåãèñòðèðîâàòü ElasticSearch
+    /// </summary>
+    private static void RegisterElasticSearch(IServiceCollection services, IConfiguration configuration)
+    {
+        var nodes = configuration.GetSection("ElasticSearch:Uris").Get<string[]>().Select(w => new Uri(w)).ToArray();
+        if (!nodes.Any())
+            return;
+
+        services.AddScoped(typeof(IElasticClient), _ =>
+        {
+            var pool = new StaticConnectionPool(nodes);
+            var settings = new ConnectionSettings(pool);
+
+            return new ElasticClient(settings);
+        });
     }
 }
