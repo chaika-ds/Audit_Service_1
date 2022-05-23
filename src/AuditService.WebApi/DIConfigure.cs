827de7296952326483727d89bac79d1526e29176
kamranmusayev <k.musayev@platform.live>
2022-05-23 11:06:18 +0400

    Add Healthy Check endpoint and configuration of DI

---
+++ src/AuditService.WebApi/DIConfigure.cs
@@ -0,0 +1,9 @@
+namespace AuditService.WebApi;
+
+public static class DIConfigure
+{
+    public static IServiceCollection Configure(IServiceCollection services)
+    {
+        return services;
+    }
+}
--- src/AuditService.WebApi/Program.cs
+++ src/AuditService.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using AuditService.WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,20 +8,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+DIConfigure.Configure(builder.Services);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuditService.WebApi v1"));
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseHealthChecks("/_hc");
 app.MapControllers();
 
 app.Run();
