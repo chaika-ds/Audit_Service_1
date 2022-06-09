eb82aaf627880d6e4b8ab13a0efafccea09b50f8
chaika_ds <d.chaika@suntigo.com>
2022-06-09 14:36:39 +0300

    Add permissionsPusher

---
+++ src/AuditService.EventConsumerApp/Auth/IPermissionPusherSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AuditService.EventConsumerApp.Auth
+{
+    public interface IPermissionPusherSettings
+    {
+        public string Topic { get; }
+
+        public Guid ServiceId { get; set; }
+
+        public string ServiceName { get; set; }
+    }
+}
