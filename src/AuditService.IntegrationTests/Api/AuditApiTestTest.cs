b777975488117834c4c268947b24d905ec5a98ef
kamranmusayev <k.musayev@platform.live>
2022-06-15 12:04:37 +0400

    added audit log endpoint test to project

---
+++ src/AuditService.IntegrationTests/Api/AuditApiTestTest.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AuditService.Data.Domain.Dto.Filter;
+using AuditService.Data.Domain.Dto.Pagination;
+using AuditService.IntegrationTests.Api.Helper;
+using Xunit;
+
+namespace AuditService.IntegrationTests.Api;
+
+public class AuditApiTestTest : BaseApiTest
+{
+    [Fact]
+    public async Task Get_Audit_GetLogAsync_Return_FilteredLogsAsync()
+    {
+        var inputModel = new AuditLogFilterRequestDto()
+        {
+            Pagination =  new PaginationRequestDto()
+            {
+                PageSize = 1,
+                PageNumber = 0
+            }
+        };
+        
+        var url = $"{Localhost}/audit/log";
+        
+        await Auth.AuthenticationService();
+        
+        var response = await HttpHelper.SendAsync(url, inputModel, Auth.NodeId.ToString(), Auth.Token, HttpMethod.Get );
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+} 
---
+++ src/AuditService.IntegrationTests/Api/ReferenceApiTestTest.cs
@@ -0,0 +1,60 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AuditService.Data.Domain.Enums;
+using AuditService.IntegrationTests.Api.Helper;
+
+
+namespace AuditService.IntegrationTests.Api;
+using Xunit;
+
+public class ReferenceApiTestTest : BaseApiTest
+{
+    [Fact]
+    public async Task GET_Referance_Services_Return_AllServiceAsync()
+    {
+        await Auth.AuthenticationService();
+        
+        var url = $"{Localhost}/reference/services";
+        
+        var response = await HttpHelper.SendAsync(url, "", Auth.NodeId.ToString(), Auth.Token, HttpMethod.Get);
+        
+        var resultRequest = await response.Content.ReadAsStringAsync();
+        
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Contains(ServiceId.SSO.ToString(), resultRequest);
+        Assert.Contains(ServiceId.PAYMENT.ToString(), resultRequest);
+    }
+    
+    [Fact]
+    public async Task GET_Referance_Categories_Return_AllCategoriesAsync()
+    {
+        await Auth.AuthenticationService();
+        
+        var url = $"{Localhost}/reference/categories";
+        
+        var response = await HttpHelper.SendAsync(url, "", Auth.NodeId.ToString(), Auth.Token, HttpMethod.Get);
+        
+        var resultRequest = await response.Content.ReadAsStringAsync();
+        
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Contains(ServiceId.SSO.ToString(), resultRequest);
+        Assert.Contains(ServiceId.PAYMENT.ToString(), resultRequest);
+    }
+    
+    [Fact]
+    public async Task GET_Referance_Categories_Return_AllCategoriesByServiceAsync()
+    {
+        await Auth.AuthenticationService();
+        
+        var url = $"{Localhost}/reference/categories/{ServiceId.PAYMENT.ToString()}";
+        
+        var response = await HttpHelper.SendAsync(url, "", Auth.NodeId.ToString(), Auth.Token, HttpMethod.Get);
+        
+        var resultRequest = await response.Content.ReadAsStringAsync();
+        
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Contains(ServiceId.PAYMENT.ToString(), resultRequest);
+    }
+    
+}
