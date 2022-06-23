fc7489dccfb1918c77cecc057c2ce381d48784e6
kamranmusayev <k.musayev@platform.live>
2022-06-23 11:02:04 +0400

    add cache support for AuthenticateMiddleware

--- src/AuditService.Setup/Middleware/AuthenticateMiddleware.cs
+++ src/AuditService.Setup/Middleware/AuthenticateMiddleware.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AuditService.Common.Enums;
+using AuditService.Setup.Extensions;
+using Microsoft.AspNetCore.Http;
+using Nest;
+using Newtonsoft.Json;
 using Tolar.Authenticate;
+using Tolar.Authenticate.Dtos;
+using Tolar.Redis;
 
 namespace AuditService.Setup.Middleware
 {
@@ -10,19 +16,26 @@ namespace AuditService.Setup.Middleware
     {
         private readonly RequestDelegate _next;
         private readonly IAuthenticateService _authenticateService;
+        private readonly IRedisRepository _redis;
+
+        private string _token = string.Empty;
+        private Guid _nodeId;
 
 
-        public AuthenticateMiddleware(RequestDelegate next, IAuthenticateService authenticateService)
+        public AuthenticateMiddleware(RequestDelegate next, IAuthenticateService authenticateService,
+            IRedisRepository redis)
         {
             _next = next;
             _authenticateService = authenticateService;
+            _redis = redis;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
-            if (!(context.Request.Path.HasValue && context.Request.Path.Value.Equals("/_hc", StringComparison.InvariantCultureIgnoreCase)))
+            if (!(context.Request.Path.HasValue &&
+                  context.Request.Path.Value.Equals("/_hc", StringComparison.InvariantCultureIgnoreCase)))
                 await AuthenticateAsync(context);
-            
+
             await _next(context);
         }
 
@@ -32,19 +45,60 @@ namespace AuditService.Setup.Middleware
         /// <param name="context">Current context</param>
         private async Task AuthenticateAsync(HttpContext context)
         {
+            var isTokenFromCache = true;
+
+            var cachedTokenKey = (nameof(AuthenticateMiddleware) + _authenticateService.NodeId).GetHash(HashType.MD5);
+            var cachedToken = await _redis.GetAsync<string>(cachedTokenKey);
+
+            if (cachedToken == null)
+            {
+                await AuthenticationServiceAsync(context);
+                isTokenFromCache = false;
+            }
+
+            var cachedUserKey = (nameof(AuthenticateMiddleware) + _nodeId + _token).GetHash(HashType.MD5);
+            var cachedUser = await _redis.GetAsync<AuthenticatedResponse>(cachedUserKey);
+
+            if (cachedUser == null)
+            {
+                var user = await GetIsUserAuthenticateAsync(context, isTokenFromCache, cachedTokenKey);
+
+                await _redis.SetAsync(cachedUserKey, JsonConvert.SerializeObject(user), TimeSpan.FromMinutes(user.TokenLifeTimeInMinutes));
+
+                await _redis.SetAsync(cachedTokenKey, JsonConvert.SerializeObject(_token), TimeSpan.FromMinutes(user.TokenLifeTimeInMinutes));
+            }
+        }
+
+        private async Task AuthenticationServiceAsync(HttpContext context)
+        {
             await _authenticateService.AuthenticationService();
 
-            var token = context.Request.Headers["Token"];
-            if (string.IsNullOrEmpty(token))
-                throw new ArgumentNullException(nameof(token), "Token is null");
+            _token = context.Request.Headers["Token"].ToString();
+            if (string.IsNullOrEmpty(_token))
+                throw new ArgumentNullException(nameof(_token), "Token is null");
 
-            var xNodeId = context.Request.Headers["X-Node-Id"];
-            if (!Guid.TryParse(xNodeId, out var nodeId))
+            var xNodeId = context.Request.Headers["X-Node-Id"].ToString();
+            if (!Guid.TryParse(xNodeId, out _nodeId))
                 throw new ArgumentNullException(nameof(xNodeId), "X-Node-Id is null");
+        }
 
-            var user = await _authenticateService.GetIsUserAuthenticate(token, nodeId);
-            if (user.Status != 200)
+        private async Task<AuthenticatedResponse> GetIsUserAuthenticateAsync(HttpContext context, bool isTokenFromCache, string cachedTokenKey)
+        {
+            var user = await _authenticateService.GetIsUserAuthenticate(_token, _nodeId);
+
+            if (user.Status == 200)
+                return user;
+
+            if (!isTokenFromCache)
                 throw new UnauthorizedAccessException("Failed to log in to the SSO");
+
+            await _redis.DeleteAsync(cachedTokenKey);
+
+            await AuthenticationServiceAsync(context);
+
+            user = await GetIsUserAuthenticateAsync(context, isTokenFromCache, cachedTokenKey);
+
+            return user;
         }
     }
 }
