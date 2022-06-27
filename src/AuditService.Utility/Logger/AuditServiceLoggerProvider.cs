9959d71379b051aa9069e0b1a617dd852d61ea00
a.kapustin <a.kapustin@platform.live>
2022-06-27 16:32:44 +0000

    Feature/log 39

--- src/AuditService.Utility/Logger/AuditServiceLoggerProvider.cs
+++ src/AuditService.Utility/Logger/AuditServiceLoggerProvider.cs
@@ -1,5 +1,4 @@
-﻿using AuditService.Utility.Logging;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Collections.Concurrent;
 using System.Runtime.Versioning;
