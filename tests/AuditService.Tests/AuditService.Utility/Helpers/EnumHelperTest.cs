6b03750f597774536e70b75375ae0a3aca1a99d9
kamranmusayev <k.musayev@platform.live>
2022-06-29 14:35:55 +0400

    Enum Helper test added to project

---
+++ tests/AuditService.Tests/AuditService.Utility/Helpers/EnumHelperTest.cs
@@ -0,0 +1,23 @@
+using AuditService.Common.Enums;
+using AuditService.Utility.Helpers;
+
+namespace AuditService.Tests.AuditService.Utility.Helpers;
+
+public class EnumHelperTest
+{
+    [Theory]
+    [InlineData("uat", LogChannel.uat)]
+    [InlineData("development", LogChannel.development)]
+    [InlineData("test", LogChannel.test)]
+    [InlineData("demo", LogChannel.demo)]
+    [InlineData("production", LogChannel.production)]
+    [InlineData("randomChannel", LogChannel.wrongChannel)]
+    public void CheckAndParseChannelTest(string value, LogChannel expResult)
+    {
+        //Arrange && Act 
+        var result = EnumHelper.CheckAndParseChannel(value);
+        
+        //Asserts
+        Assert.Equal(expResult, result);
+    }
+}
