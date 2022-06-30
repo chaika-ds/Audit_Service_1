e70c8324b84985500b1659e67a6782a4d2d60f15
kamranmusayev <k.musayev@platform.live>
2022-06-30 10:44:39 +0400

    Added unit test for JsonHelper

--- tests/AuditService.Tests/AuditService.Utility/Helpers/EnumHelperTest.cs
+++ tests/AuditService.Tests/AuditService.Utility/Helpers/EnumHelperTest.cs
@@ -3,8 +3,14 @@ using AuditService.Utility.Helpers;
 
 namespace AuditService.Tests.AuditService.Utility.Helpers;
 
+/// <summary>
+/// Unit Test for EnumHelper class
+/// </summary>
 public class EnumHelperTest
 {
+    /// <summary>
+    /// Unit Test for CheckAndParseChannel method
+    /// </summary>
     [Theory]
     [InlineData("uat", LogChannel.uat)]
     [InlineData("development", LogChannel.development)]
