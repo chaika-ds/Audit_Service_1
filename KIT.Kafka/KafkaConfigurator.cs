d7e0f6eaef1a615e56e2e33cc147cf948289d8d8
Tanis_Still <shaman14061995@gmail.com>
2022-07-04 18:43:09 +0600

    Removed legacy consoleApp

--- KIT.Kafka/KafkaConfigurator.cs
+++ KIT.Kafka/KafkaConfigurator.cs
@@ -18,13 +18,35 @@ namespace KIT.Kafka
         /// <param name="services">Services сollection</param>
         public static void ConfigureKafka(this IServiceCollection services)
         {
+            services.AddKafkaSettings().AddKafkaServices();
+        }
+
+        /// <summary>
+        /// Add settings for working with kafka
+        /// </summary>
+        /// <param name="services">Services сollection</param>
+        /// <returns>Services сollection</returns>
+        private static IServiceCollection AddKafkaSettings(this IServiceCollection services)
+        {
             services.AddSettings<IKafkaSettings, KafkaSettings>();
             services.AddSettings<IPermissionPusherSettings, PermissionPusherSettings>();
             services.AddSettings<IKafkaTopics, KafkaTopics>();
 
+            return services;
+        }
+
+        /// <summary>
+        /// Add services for working with Kafka
+        /// </summary>
+        /// <param name="services">Services сollection</param>
+        /// <returns>Services сollection</returns>
+        private static IServiceCollection AddKafkaServices(this IServiceCollection services)
+        {
             services.AddSingleton<IKafkaConsumerFactory, KafkaConsumerFactory>();
             services.AddSingleton<IKafkaProducer, KafkaProducer>();
             services.AddHostedService<PushPermissionBackgroundService>();
+
+            return services;
         }
     }
 }
