8e17ac77a434ec7379007c327161f6ff90996bae
kamranmusayev <k.musayev@platform.live>
2022-07-11 11:25:33 +0400

    create BaseAuditLogModel for all audit log child models

--- src/AuditService.Common/Models/Domain/AuditLogTransactionDomainModel.cs
+++ src/AuditService.Common/Models/Domain/AuditLogTransactionDomainModel.cs
@@ -6,92 +6,12 @@ namespace AuditService.Common.Models.Domain;
 /// <summary>
 ///     Audit log transaction
 /// </summary>
-public class AuditLogTransactionDomainModel
+public class AuditLogTransactionDomainModel : BaseAuditLogModel
 {
-    public AuditLogTransactionDomainModel()
-    {
-        CategoryCode = string.Empty;
-        RequestUrl = string.Empty;
-        RequestBody = string.Empty;
-        EntityName = string.Empty;
-        OldValue = string.Empty;
-        NewValue = string.Empty;
-        User = new IdentityUserDomainModel();
-    }
-
     /// <summary>
     ///     Service ID
     /// </summary>
     [Required]
     public ServiceStructure Service { get; set; }
 
-    /// <summary>
-    ///     ID of the node where the change occurred
-    /// </summary>
-    [Required]
-    public Guid NodeId { get; set; }
-
-    /// <summary>
-    ///     Node Type
-    /// </summary>
-    [Required]
-    public NodeType Node { get; set; }
-
-    /// <summary>
-    ///     Type of action
-    /// </summary>
-    [Required]
-    public ActionType Action { get; set; }
-
-    /// <summary>
-    ///     Category of actions (depending on modules)
-    /// </summary>
-    [Required]
-    public string CategoryCode { get; set; }
-
-    /// <summary>
-    ///     The text representation of the request
-    /// </summary>
-    public string RequestUrl { get; set; }
-
-    /// <summary>
-    ///     The JSON representation of the request
-    /// </summary>
-    public string RequestBody { get; set; }
-
-    /// <summary>
-    ///     Date and time of the event (ISO 8601 UTC standard)
-    /// </summary>
-    [Required]
-    public DateTime Timestamp { get; set; }
-
-    /// <summary>
-    ///     The name of the class (or table in the database) of the logged entity
-    /// </summary>
-    public string EntityName { get; set; }
-
-    /// <summary>
-    ///     ID of the logged entity (possible types of UUID/Long values)
-    /// </summary>
-    public Guid EntityId { get; set; }
-
-    /// <summary>
-    ///     JSON representation of the previous value of the entity
-    /// </summary>
-    public string OldValue { get; set; }
-
-    /// <summary>
-    ///     JSON representation of a new entity value
-    /// </summary>
-    public string NewValue { get; set; }
-
-    /// <summary>
-    ///     ID of the project from which the change is logged in the audit
-    /// </summary>
-    public Guid ProjectId { get; set; }
-
-    /// <summary>
-    ///     User
-    /// </summary>
-    public IdentityUserDomainModel User { get; set; }
 }
