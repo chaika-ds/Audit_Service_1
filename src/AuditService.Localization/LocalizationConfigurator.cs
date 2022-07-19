a8a6548de9079e621690064078ebcf9c6671c305
n.avdeichik <n.avdeichik@platform.live>
2022-07-19 10:08:19 +0000

    Resolve LOG-107 "Feature/n.avdeichik/ localization"

---
+++ src/AuditService.Localization/LocalizationConfigurator.cs
@@ -0,0 +1,27 @@
+﻿using AuditService.Localization.Localizer;
+using AuditService.Localization.Localizer.Source;
+using AuditService.Localization.Localizer.Storage;
+using AuditService.Localization.Settings;
+using bgTeam.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuditService.Localization;
+
+/// <summary>
+///     Localization configurator
+/// </summary>
+public static class LocalizationConfigurator
+{
+    /// <summary>
+    ///     Configure Localization. Register services and settings.
+    /// </summary>
+    /// <param name="services">Services сollection</param>
+    public static void ConfigureLocalization(this IServiceCollection services)
+    {
+        services.AddSettings<IRedisCacheStorageSettings, RedisCacheStorageSettings>();
+        services.AddSettings<ILocalizationSourceSettings, LocalizationSourceSettings>();
+        services.AddScoped<ILocalizationStorage, RedisCacheStorage>();
+        services.AddScoped<ILocalizationSource, LocalizationService>();
+        services.AddScoped<ILocalizer, Localizer.Localizer>();
+    }
+}
---
+++ src/AuditService.Localization/Settings/IRedisCacheStorageSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace AuditService.Localization.Settings;
+
+/// <summary>
+///     Redis storage settings for storing localization resources
+/// </summary>
+public interface IRedisCacheStorageSettings
+{
+    /// <summary>
+    ///     Cache lifetime in minutes
+    /// </summary>
+    int CacheLifetimeInMinutes { get; set; }
+}
