98c5b34eeb51475b3e8c44f33c4c2224f47a04bb
kamranmusayev <k.musayev@platform.live>
2022-08-12 18:13:20 +0400

    added HealthCheckRequestHandlerTest

---
+++ tests/AuditService.Tests/Tests/Handlers/HealthCheckRequestHandlerTest.cs
@@ -0,0 +1,53 @@
+using AuditService.Common.Models.Dto;
+using AuditService.Handlers.Handlers;
+using AuditService.Tests.Fakes.Setup.ELK;
+using KIT.Kafka.HealthCheck;
+using KIT.Redis.HealthCheck;
+using MediatR;
+using Moq;
+using Nest;
+
+namespace AuditService.Tests.Tests.Handlers;
+
+/// <summary>
+///     Allows you to get a list of available services and categories
+/// </summary>
+public class HealthCheckRequestHandlerTest
+{
+    private readonly IElasticClient _elasticClient;
+
+    public HealthCheckRequestHandlerTest()
+    {
+        _elasticClient = ElasticSearchClientProviderFake.GetFakeElasticSearchClient("");
+    }
+
+    /// <summary>
+    ///     Testing Handle Method
+    /// </summary>
+    [Fact]
+    public async Task CheckHandle_Result_EachServiceCalledOnce()
+    {
+        var mediatorMock = new Mock<IMediator>();
+        var kafkaHcMock = new Mock<IKafkaHealthCheck>();
+        var redisHcMock = new Mock<IRedisHealthCheck>();
+
+
+        kafkaHcMock.Setup(x => x.CheckHealthAsync(CancellationToken.None)).Returns(Task.FromResult(new HealthCheckComponentsDto()));
+        redisHcMock.Setup(x => x.CheckHealthAsync(CancellationToken.None)).Returns(Task.FromResult(new HealthCheckComponentsDto()));
+        mediatorMock.Setup(x => x.Send(It.IsAny<GitLabRequest>(), CancellationToken.None))
+            .Returns(Task.FromResult(new GitLabVersionResponseDto()
+            {
+                Branch = "Master",
+                Commit = "Commit",
+                Tag = "Tag"
+            }));
+        
+        var handle = new HealthCheckRequestHandler(mediatorMock.Object, _elasticClient, kafkaHcMock.Object, redisHcMock.Object);
+        await handle.Handle(new CheckHealthRequest(), CancellationToken.None);
+
+        
+        kafkaHcMock.Verify(x =>x.CheckHealthAsync(CancellationToken.None), Times.Once());
+        redisHcMock.Verify(x => x.CheckHealthAsync(CancellationToken.None), Times.Once());
+        mediatorMock.Verify(x => x.Send(It.IsAny<GitLabRequest>(), CancellationToken.None), Times.Once());
+    }
+}
